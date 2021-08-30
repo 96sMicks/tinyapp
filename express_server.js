c27c8dbbeb3a0cf1ed1e1004082b014facd965f6
96sMicks <melkiasae@hotmail.com>
2021-08-30 23:44:16 +0000

    Added another path for shortURL

--- express_server.js
+++ express_server.js
@@ -22,6 +22,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req)
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
